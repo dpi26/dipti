b5eb3e295121b9c4099881fcc29ac2161c6da0c6
dpi26 <kdipti099@gmail.com>
2024-09-03 02:50:09 -0700

    Add files via upload

---
+++ gs8.js
@@ -0,0 +1,45 @@
+function myfunction(){
+    console.log("welcome to home");//paramerter
+    console.log("we are indian");
+}  
+myfunction();   //function calling
+
+
+function mefunction(msg) {
+    console.log(msg );
+}
+ mefunction("love",);//argument
+
+ //2no.sum
+
+ //function sum(x,y){
+   // console.log(x+y);
+ //}
+ function sum(x,y){
+    s = x + y;
+    console.log("before return");
+    return s;
+ }                                  
+          
+                    //arrowfunction
+
+
+
+ let v=sum(4,56);
+ console.log(v);
+
+ const arrowSum=(a,b)=>{
+    console.log(a+b);
+ };
+ //multiple
+ function mul(a,b){
+    return a*b;
+ }
+ const arrowMul=(a,b)=>{
+    console.log(a*b);
+
+ };
+ const printhellow=()=>{
+    console.log("hellow");
+ };
+ 
