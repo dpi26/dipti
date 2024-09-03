eebb4c44b6727b546d08d041fdb55dca79d92b75
dpi26 <kdipti099@gmail.com>
2024-09-03 02:24:33 -0700

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void main()
+{
+
+ //print pattern in right
+
+int i,j;
+for ( i = 1; i <=4; i++)                   
+{
+    for ( j =1; j <=5; j++)
+    {
+        printf("%c ",j+64);
+    }
+    printf("\n");
+}
+
+ //print pattern in oposite 
+
+for (int x = 4; x>=1; x--)           
+{
+    for (int y=4; y >=1; y--)
+    {
+    printf("%d ",x);
+    }
+    printf("\n");
+  
+}
+
+//pattern in trianglr
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
