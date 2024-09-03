590e66d411aeebc0fa390535bf53ba2a3a9c527c
dpi26 <kdipti099@gmail.com>
2024-09-03 02:10:23 -0700

    Add files via upload

---
+++ pra1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    char B,a,b;  
+      printf("enter the input value:");
+scanf("%c",B);
+if(B==a){
+    printf("hellow");
+}
+else if(B!=a){
+    printf("world");
+}
+  else{
+    printf("program");
+}
+return 0;
+}   
